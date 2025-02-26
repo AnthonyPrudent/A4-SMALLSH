fdb1dc417ea0b555fb369aa7354148b46a151b97
AnthonyPrudent <146302573+AnthonyPrudent@users.noreply.github.com>
2025-02-26 20:41:05 +0000

    Implemented cd

--- prudenta_assignment4.c
+++ prudenta_assignment4.c
@@ -6,14 +6,19 @@
 
 void ls() 
 {
-    DIR* current_directory = opendir(".");
+    DIR* curr_directory = opendir(".");
     struct dirent* entry;
     
-    while((entry = readdir(current_directory)) != NULL)
+    while((entry = readdir(curr_directory)) != NULL)
     {   
-        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
-        printf("%s  ", entry->d_name);
-        fflush(stdout);
+
+        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+           
+            printf("%s  ", entry->d_name);
+            fflush(stdout);
+
+        }
+
     }
 
     printf("\n");
@@ -35,11 +40,13 @@ void ls()
 
 struct command_line
 {
+
     char *argv[MAX_ARGS + 1];
     int argc;
     char *input_file;
     char *output_file;
     bool is_bg;
+
 };
  
  
@@ -58,34 +65,107 @@ struct command_line *parse_input()
     char *token = strtok(input, " \n");
     while(token) {
         if(!strcmp(token,"<")) {
+
             curr_command->input_file = strdup(strtok(NULL," \n"));
+
         } else if(!strcmp(token,">")) {
+
              curr_command->output_file = strdup(strtok(NULL," \n"));
+
         } else if(!strcmp(token,"&")) {
+
              curr_command->is_bg = true;
+
         } else {
+
              curr_command->argv[curr_command->argc++] = strdup(token);
+
         }
+
          token=strtok(NULL," \n");
+
     }
+
     return curr_command;
+
 }
  
+void cd(struct command_line *curr_command)
+{   
+    char curr_abs_directory[INPUT_LENGTH];
+    char curr_rel_directory[INPUT_LENGTH];
+    DIR* opened_directory;
+    struct dirent *entry;
+
+    strcpy(curr_abs_directory, getenv("PWD"));
+    strcpy(curr_rel_directory, strrchr(curr_abs_directory, '/'));
+
+    printf("%s\n", curr_abs_directory);
+
+    // CD with no arguments handling
+    if(curr_command->argc == 1) {
+
+        setenv("PWD", getenv("HOME"), 1);
+    
+    } else if(strcmp(curr_command->argv[1], ".") != 0) {
+
+        // Relative path handling with dots
+        if(strcmp(curr_command->argv[1], "..") == 0 || (strcmp(curr_command->argv[1], "../") == 0)) {
+            
+            curr_abs_directory[strlen(curr_abs_directory) - strlen(curr_rel_directory)] = '\0';
+
+            setenv("PWD", curr_abs_directory, 1);
+        
+        // Absolute path handling
+        } else if(opendir(curr_command->argv[1]) != NULL) {
+
+            setenv("PWD", curr_command->argv[1], 1);
+        
+        // Relative path handling with directory name
+        } else {
+
+            opened_directory = opendir(getenv("PWD"));
+
+            while((entry = readdir(opened_directory)) != NULL) {
+
+                if(strcmp(entry->d_name, curr_command->argv[1]) == 0) {
+                    
+                    strcat(curr_abs_directory, "/");
+                    strcat(curr_abs_directory, curr_command->argv[1]);
+                    setenv("PWD", curr_abs_directory, 1);
+
+                }
+
+                closedir(opened_directory);
+
+            }
+        
+        }
+
+    }
+
+    printf("%s\n", getenv("PWD"));
+
+}
+
 int main()
 {
+
     struct command_line *curr_command;
 
     while(true)
     {
-        curr_command = parse_input();
 
-        for(int i=0; i < curr_command->argc; i++)
-        {
-            if(strcmp(curr_command->argv[i], "ls") == 0) {
-                ls();
-            }
+        curr_command = parse_input();
+        
+        if(strcmp(curr_command->argv[0], "cd") == 0) {
+
+            cd(curr_command);
+
         }
 
     }
+
     return EXIT_SUCCESS;
+
 }
